bb2d048566c71145ac7cb92e8906406301bba1c4
bombasic <alenbasic3@gmail.com>
2022-02-08 18:28:56 -0500

    Adding initial Meals components with dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
